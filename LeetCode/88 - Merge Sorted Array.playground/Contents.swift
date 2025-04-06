a04f02af07b440a4a5c36f34ecad603e77a2f56c
Vanderlei <vandlopes98@gmail.com>
2025-04-06 20:11:37 -0300

    resolving merge sorted array

---
+++ LeetCode/88 - Merge Sorted Array.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+//
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// O(m+n) approach with pointers
+class Solution {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var i = m - 1
+        var j = n - 1
+        var k = m + n - 1
+
+        while j >= 0 {
+            if i >= 0 && nums1[i] > nums2[j] {
+                nums1[k] = nums1[i]
+                i -= 1
+            } else {
+                nums1[k] = nums2[j]
+                j -= 1
+            }
+
+            k -= 1
+        }
+    }
+}
